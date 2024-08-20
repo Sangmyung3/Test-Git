8e19de9e41e28fc44f1a7084dd09b2c5e4ff23c4
Sangmyung3 <118576161+Sangmyung3@users.noreply.github.com>
2024-08-20 13:21:12 -0400

    update VECM

--- functions_VECM.py
+++ functions_VECM.py
@@ -1,1 +1,3 @@
 ## Here, I store functions needed to estimate VECM
+
+## function: AIC, BIC for me
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 ### Estimate VECM and do structural shock identification
 
 ### Make some changes here
+
+# Do another shock identification
